3097f8ffbdecdcf2d2b82ca6a0417259cbbcea35
Luca Barbato <lu_zero@gentoo.org>
2010-09-28 14:02:54 +0200

    Main cleanup

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set here your own NDK path if needed
-# export PATH=$PATH:~/src/endless_space/android-ndk-r4b
-# Set environment to CrystaX NDK with RTTI and exceptions instead of original NDK
-# export PATH=$PATH:~/src/endless_space/android-ndk-r4-crystax/ndk-build
-
-cd project && nice -n5 ndk-build -j2 V=1 && ant debug && cd bin && adb install -r DemoActivity-debug.apk
-
--- project/themes/converter/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for f in ../UltimateDroid/*.png; do
-	newname=`echo $f | sed 's@.*/@@' | tr '[A-Z]' '[a-z]'`.raw
-	./converter $f ../../res/raw/$newname
-	gzip -9 < ../../res/raw/$newname > ../../res/raw/$newname.gz
-	mv -f ../../res/raw/$newname.gz ../../res/raw/$newname
-done
-
